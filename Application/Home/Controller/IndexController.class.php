0f3d57ca4f8555bb423c29767f790c58a256c184
ykc <907148778@qq.com>
2016-06-29 19:27:09 +0800

    增加首次关注时的推送

--- Application/Home/Controller/IndexController.class.php
+++ Application/Home/Controller/IndexController.class.php
@@ -4,17 +4,79 @@ use Think\Controller;
 class IndexController extends Controller {
     public function index(){
         if(IS_GET){
+            /**
+             * 1.获得参数  timestamp,nonce,token,signature,echostr
+             * 2.行程数组
+             * 3.排序
+             * 4.拼接字符串
+             * 5.加密
+             * 6.比较
+             */
             $timestamp  =  I('get.timestamp','');
             $nonce      =  I('get.nonce','');
             $token      =  'weixin';
             $signature  =  I('get.signature','');
+            $echosr     =  I('get.echostr');
             $arr        =  array($timestamp,$nonce,$token);
             sort($arr);
             $tmpstr     =  implode('',$arr );
             $tmpstr     =  sha1($tmpstr);
-            if ($tmpstr == $signature){
-                echo I('get.echostr','none');
+
+            /**
+             * 只有第一次的时候才会验证，所以，不是第一次，直接跳过
+             */
+            if ($tmpstr == $signature && $echosr){
+                echo $echosr;
                 exit;
+            }
+            else{
+                $this->responseMsg();
+            }
+        }
+    }
+    public function responseMsg(){
+        /**
+         * 1.获取到微信推送过来的post数据（xml）
+                <xml>
+                <ToUserName><![CDATA[toUser]]></ToUserName>
+                <FromUserName><![CDATA[FromUser]]></FromUserName>
+                <CreateTime>123456789</CreateTime>
+                <MsgType><![CDATA[event]]></MsgType>
+                <Event><![CDATA[subscribe]]></Event>
+                </xml>
+         *2.判断该数据包是否是订阅的事件推送
+         * 3.回复用户
+                <xml>
+                <ToUserName><![CDATA[toUser]]></ToUserName>
+                <FromUserName><![CDATA[fromUser]]></FromUserName>
+                <CreateTime>12345678</CreateTime>
+                <MsgType><![CDATA[text]]></MsgType>
+                <Content><![CDATA[你好]]></Content>
+                </xml>
+         */
+        $postArr                =   $GLOBALS['HTTP_RAW_POST_DATA'];
+        $postObj                =   simplexml_load_string($postArr);
+        if (strtolower($postObj->MsgType) == 'event'){
+            //判断是否是订阅时间
+            if (strtolower($postObj->Event) == 'subscribe'){
+                //回复用户消息
+                $toUser         =   $postObj->FromUserName;
+                $fromUser       =   $postObj->toUserName;
+                $time           =   time();
+                $Msgtype        =   'text';
+                $content        =   '欢迎关注，我是夜微凉';
+                $template       =   "
+                               <xml>
+                               <ToUserName><![CDATA[%s]]></ToUserName>
+                               <FromUserName><![CDATA[%s]]></FromUserName>
+                               <CreateTime>%s</CreateTime>
+                               <MsgType><![CDATA[%s]]></MsgType>
+                               <Content><![CDATA[%s]]></Content>
+                               </xml>
+                ";
+                $info           =   sprintf($template,$toUser,$fromUser,$time,$Msgtype,$content);
+                echo $info;
+
             }
         }
     }
