3f1adcb075eb6ec4b720f3b3f6d9a80958ac763d
ykc <907148778@qq.com>
2016-07-01 09:08:42 +0800

    修改天气查询

--- Application/Home/Controller/IndexController.class.php
+++ Application/Home/Controller/IndexController.class.php
@@ -105,7 +105,7 @@ class IndexController extends Controller {
             $fromUser   =   $postObj->ToUserName;
             $toUser     =   $postObj->FromUserName;
             $time       =   time();
-            $Msg        =   "欢迎关注，我是夜微凉";
+            $Msg        =   "祝您生活愉快";
             //准备回复
             $template   =   "
                         <xml>
@@ -127,18 +127,58 @@ class IndexController extends Controller {
         }
     }
     protected function getWeather($weather){
+
         $preg       =   "/天气$/i";
         $match      =   preg_match($preg,$weather);
         if (!$match){
             return false;
         }
         preg_match_all('/(.*)天气$/i',$weather,$match);
-        $city       =   $match[1];
-        $url        =   "http://wthrcdn.etouch.cn/weather_mini?city=";
-        $weatherMsg =   file_get_contents($url.$city);
-        $weatherMsg =   json_decode($weatherMsg);
-        $Msg        =   $weatherMsg['data']['forecast'];
-        $Msg        =   implode('\n',$Msg );
+        $city       =   $match[1][0];
+
+//        $url        =   "http://wthrcdn.etouch.cn/weather_mini?city=";
+//        $weatherMsg =   file_get_contents($url.$city);
+//        $weatherMsg =   json_decode($weatherMsg);
+//        $Msg        =   $weatherMsg['data']['forecast'];
+//        $Msg        =   implode('\n',$Msg );
+//        return $Msg;
+
+        $ch = curl_init();
+        $url = 'http://apis.baidu.com/heweather/weather/free?city='.$city;
+        $header = array(
+            'apikey: 3bdb311a33696ccdc5780f8032ac5e26',
+        );
+        // 添加apikey到header
+        curl_setopt($ch, CURLOPT_HTTPHEADER  , $header);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        // 执行HTTP请求
+        curl_setopt($ch , CURLOPT_URL , $url);
+        $res = curl_exec($ch);
+        $res = json_decode($res,true);
+//        dump($res);
+
+        $city  = $res['HeWeather data service 3.0'][0]['basic']['city'];
+        $time  = $res['HeWeather data service 3.0'][0]['basic']['update']['loc'];
+        $pm25  = $res['HeWeather data service 3.0'][0]['aqi']['city']['pm25'];
+        $qlty  = $res['HeWeather data service 3.0'][0]['aqi']['city']['qlty'];
+        $txt_d = $res['HeWeather data service 3.0'][0]['daily_forecast'][0]['cond']['txt_d'];
+        $txt_n = $res['HeWeather data service 3.0'][0]['daily_forecast'][0]['cond']['txt_n'];
+        $max   = $res['HeWeather data service 3.0'][0]['daily_forecast'][0]['tmp']['max'];
+        $min   = $res['HeWeather data service 3.0'][0]['daily_forecast'][0]['tmp']['min'];
+
+
+        $Msg   = array(
+            '城市'        => $city,
+            '时间'        => $time,
+            'pm2.5'      => $pm25,
+            '空气质量'    =>  $qlty,
+            '天气'        =>  $txt_d,
+            '转'         =>   $txt_n,
+            '最高气温'    =>    $max,
+            '最低气温'      =>  $min
+        );
+        $Msg = "城市：".$city."\n"."时间:".$time."\n"."pm2.5：".$pm25."\n"."空气质量：".$qlty."\n"."天气：".$txt_d."转".$txt_n."\n"."最高气温：".$max."\n"."最低气温".$min;
         return $Msg;
     }
+
 }
